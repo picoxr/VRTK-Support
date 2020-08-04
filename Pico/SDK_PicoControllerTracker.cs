9f00955c63b7d9a9dcee4955230010c4d720e2e0
picoxr <support@picovr.com>
2020-08-04 14:25:22 +0800

    init

---
+++ Pico/SDK_PicoControllerTracker.cs
@@ -0,0 +1,68 @@
+﻿// Unity SDK Controller Tracker|SDK_Unity|005
+namespace VRTK
+{
+    using UnityEngine;
+#if VRTK_DEFINE_SDK_PICO
+    using Pvr_UnitySDKAPI;
+#endif
+#if UNITY_2017_2_OR_NEWER
+    using UnityEngine.XR;
+#else
+    using UnityEngine.VR;
+    using XRNode = UnityEngine.VR.VRNode;
+#endif
+
+    /// <summary>
+    /// The Controller Tracker enables the GameObject to track it's position/rotation to the available connected VR Controller via the `UnityEngine.VR` library.
+    /// </summary>
+    /// <remarks>
+    /// The Unity Controller Tracker is attached to the `[UnityBase_CameraRig]` prefab on the child `LeftHandAnchor` and `RightHandAnchor` to enable controller tracking.
+    /// </remarks>
+    public class SDK_PicoControllerTracker : MonoBehaviour
+    {
+        [Tooltip("The Unity VRNode to track.")]
+        public XRNode nodeType;
+        [Tooltip("The unique index to assign to the controller.")]
+        public uint index;
+        [HideInInspector, Tooltip("The Unity Input name for the trigger axis.")]
+        public string triggerAxisName = "";
+        [HideInInspector, Tooltip("The Unity Input name for the grip axis.")]
+        public string gripAxisName = "";
+        [HideInInspector, Tooltip("The Unity Input name for the touchpad horizontal axis.")]
+        public string touchpadHorizontalAxisName = "";
+        [HideInInspector, Tooltip("The Unity Input name for the touchpad vertical axis.")]
+        public string touchpadVerticalAxisName = "";
+
+        protected virtual void OnEnable()
+        {
+            CheckAxisIsValid(triggerAxisName, "triggerAxisName");
+            CheckAxisIsValid(gripAxisName, "gripAxisName");
+            CheckAxisIsValid(touchpadHorizontalAxisName, "touchpadHorizontalAxisName");
+            CheckAxisIsValid(touchpadVerticalAxisName, "touchpadVerticalAxisName");
+        }
+
+        protected virtual string GetVarName<T>(T item) where T : class
+        {
+            return VRTK_SharedMethods.GetPropertyFirstName<T>();
+        }
+
+        protected virtual void CheckAxisIsValid(string axisName, string varName)
+        {
+            //try
+            //{
+            //    Input.GetAxis(axisName);
+            //}
+            //catch (System.ArgumentException ae)
+            //{
+            //    VRTK_Logger.Warn(ae.Message + " on index [" + index + "] variable [" + varName + "]");
+            //}
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            //transform.localPosition = transform.InverseTransformDirection(Controller.UPvr_GetControllerPOS((int)index));
+            //transform.localRotation = Controller.UPvr_GetControllerQUA((int)index);
+            Debug.Log($"Controller {index} position is {transform.localPosition} and rotation is {transform.localRotation}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Integrated VRTK
- 
+
+## Unity Versions
+
+Unity2019.3.6
+
+## Description
+
+There are seven scenes which are already configured in Assets-VRTK-Examples".
+![](1.jpg)
+
+## Note
+
+Please keep in mind that the guide has to be executed in this order. If not, then the integration might not be as desired. 
+VRTK should already be installed and you need access to the files given in this folder. It's tested in Unity 2019.2.2f1 and confirmed to work in that version. 
+Not all features might be implemented. These features can be found in the provided VRTK demo scenes. 
+Also, be sure to make a backup (or two) beforehand.
+
+## Installation
+
+1. Import Pico SDK and apply recommended settings. This will also set the build target to Android and if not set already, it can take a long time depending on the project size. 
+2. Copy the "Pico" folder (located in the SDK folder) into your projects VRTK->Source->SDK folder.
+3. Copy the "PicoVR.prefab" file into your projects prefab folder.
+4. Close Unity.
+5. Replace the "VRTK_SDKManager.cs" file in the VRTK -> Source -> Scripts -> Utilities -> SDK folder with the existing file (in order to let VRTK detect the Pico headset) while Unity is closed!
+6. Reopen the Unity project.
+7. Drag PicoVR prefab into your scenes [VRTK_SDKManager] -> [VRTK_SDKSetups].
+8. Select [VRTK_SDKManager] and add the PicoVR game object that you just placed into the setups list.
+9. Click on the “Remove All Symbols" button in the [VRTK_SDKManager]. 
+10. Uncheck “Auto Manager”, click on the “Manage Now" button and check the “Auto Manage" once again.
+11. Go to Edit > Project settings > Player > XR Settings and add “None” as an option for VR devices.
+12. Hook up all required components for the specific application you are working on (if there are any.)
+13. You're done!
